0af677abb22d549e0727eac525a4f334a0d23b91
yueguangwu@yl-game.cn <Wuyue950322>
2018-08-21 14:32:23 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,131 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    circular: true,
+    bannerList: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    gameList: [{
+        appId: 'wx91d17ce64dd9004f',
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！开局一根棍，装备全靠打！开局一根棍，装备全靠打！',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        appId: 'wx91d17ce64dd9004f',
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！开局一根棍，装备全靠打！',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！开局一根棍，装备全靠打！开局一根棍，装备全靠打！',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！开局一根棍...',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！开局一根棍，装备全靠打！开局一根棍，装备全靠打！开局一根棍，装备全靠打！开局一根棍，装备全靠打！',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        title: '全民大乱斗开局一根棍，装备全靠打！开局一根棍，装备全靠打！',
+        desc: '开局一根棍，装备全靠打！开局一根棍，装备全靠打！开局一根棍，装备全靠打！',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！开局一根棍...',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      },
+      {
+        title: '全民大乱斗',
+        desc: '开局一根棍，装备全靠打！开局一根棍，装备全靠打！开局一根棍，装备全靠打！',
+        imageUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        amout: 100
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
